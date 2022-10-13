cddec98eabd830ccf5c84372f439376d7958a595
allen dean <allen7242@gmail.com>
2022-10-13 09:41:04 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,31 @@
+import "./style.css";
+import axios from "axios";
+
+const options = {
+  method: "GET",
+  url: "https://mental-health-info-api.p.rapidapi.com/news/thetimes",
+  headers: {
+    "X-RapidAPI-Key": "5ef809a694msh5351997d6edb97fp140c58jsnd8274cffe3a8",
+    "X-RapidAPI-Host": "mental-health-info-api.p.rapidapi.com",
+  },
+};
+
+axios
+  .request(options)
+  .then(function (response) {
+    console.log(response.data);
+
+    document.querySelector("#app").innerHTML = `
+  <div>    
+    <h1>Mental Empowerment!</h1>
+    <h2>Our Sites</h2>
+    <h3>${response.data[0].title}</h3>
+    <a href="${response.data[0].url}">read more</a>
+      </div>
+`;
+
+  })
+  .catch(function (error) {
+    console.error(error);
+  });
+
